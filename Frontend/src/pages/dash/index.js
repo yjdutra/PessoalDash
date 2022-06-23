7b1fc019c9466de3dfe7757cc89b6feba28f9a4f
YuriJorgeD <yjdutra@gmail.com>
2022-06-23 18:03:30 -0300

    [dev] Created

--- Frontend/src/pages/dash/index.js
+++ Frontend/src/pages/dash/index.js
@@ -1,20 +1,21 @@
 import styles from "./layout.module.css";
 import CardContas from "../../components/cardContas";
 import InputCard from "../../components/input";
-
+import WebApps from "../../components/WebApps";
 
 export default function Dash() {
   return (
     <>
       <div className={styles.CardsContas}>
-        <CardContas value="150,00" type="agua"/>
+        <CardContas value="150,00" type="agua" />
         <CardContas value="200,00" type="energia" />
         <CardContas value="500,00" type="condominio" />
         <CardContas value="175,00" type="internet" />
         <CardContas value="100,00" type="gas" />
       </div>
-      <br/>
-      <InputCard />
+      <br />
+
+      {/* <InputCard /> <WebApps /> */}
     </>
   );
 }
