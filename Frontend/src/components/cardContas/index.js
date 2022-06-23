2c727b324b3c2f2be2c7c59c7d2e92941563f35c
YuriJorgeD <yjdutra@gmail.com>
2022-06-23 11:43:38 -0300

    [dev]: Front CardBills component created

--- Frontend/src/components/cardContas/index.js
+++ Frontend/src/components/cardContas/index.js
@@ -1,26 +1,77 @@
 import styles from "./layout.module.css";
 
-const Gota = (
-  <svg
-    stroke="currentColor"
-    fill="currentColor"
-    stroke-width="0"
-    viewBox="0 0 512 512"
-    height="1em"
-    width="1em"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M352 146.4c-34.4-48.6-67.5-78.5-90.8-96.6-3.1-2.4-7.3-2.4-10.4-.1-23 17.1-56.1 48.4-90.5 96.5-37.3 52-63 108.4-64.2 170.9 0 1.2-.1 2.5-.1 3.7 0 18.4 3.9 35.9 10.9 52.1 4.1 9.3 9.2 18.1 15.2 26.3 28.5 39 77.8 64.8 133.8 64.8 88.4 0 160.1-64.1 160.1-143.2 0-63.7-27-122.2-64-174.4zm-86 264.3h-.5c-9.9 0-12-14.1-2.6-17.1 45.1-14.2 69.6-38.5 86.4-80.8 3.5-8.9 16.7-6.5 16.8 3.1v1.4c-.1 51.6-44.9 93.4-100.1 93.4z"></path>
-  </svg>
-);
+import { gota, raio, predio, internet, fogo } from "../../assets/icons";
 
-export default function CardContas() {
-  return (
-    <>
-        <div className={styles.card}>
-          <p className={styles.card__title} >R$ 100,00 {Gota}</p>
-          <p className={styles.card__apply}>Conta de Água</p>
+export default function CardContas(props) {
+  const bill = props.type;
+
+  if (bill == "agua") {
+    return (
+      <>
+        <div className={styles.cardAgua}>
+          <p className={styles.card__title}>
+            R$ {props.value}
+            <span className={styles.icon}>{gota}</span>
+          </p>
+          <p className={styles.card__apply}>Água</p>
         </div>
-    </>
-  );
+      </>
+    );
+  }
+
+  if (bill == "energia") {
+    return (
+      <>
+        <div className={styles.cardEnergia}>
+          <p className={styles.card__title}>
+            R$ {props.value}
+            <span className={styles.icon}>{raio}</span>
+          </p>
+          <p className={styles.card__apply}>Energia</p>
+        </div>
+      </>
+    );
+  }
+
+  if (bill == "condominio") {
+    return (
+      <>
+        <div className={styles.cardCondominio}>
+          <p className={styles.card__title}>
+            R$ {props.value}
+            <span className={styles.icon}>{predio}</span>
+          </p>
+          <p className={styles.card__apply}>Condomínio</p>
+        </div>
+      </>
+    );
+  }
+
+  if (bill == "internet") {
+    return (
+      <>
+        <div className={styles.cardInternet}>
+          <p className={styles.card__title}>
+            R$ {props.value}
+            <span className={styles.icon}>{internet}</span>
+          </p>
+          <p className={styles.card__apply}>Internet</p>
+        </div>
+      </>
+    );
+  }
+
+  if (bill == "gas") {
+    return (
+      <>
+        <div className={styles.cardGas}>
+          <p className={styles.card__title}>
+            R$ {props.value}
+            <span className={styles.icon}>{fogo}</span>
+          </p>
+          <p className={styles.card__apply}>Gás</p>
+        </div>
+      </>
+    );
+  }
 }
--- Frontend/src/pages/dash/index.js
+++ Frontend/src/pages/dash/index.js
@@ -1,12 +1,18 @@
 import styles from "./layout.module.css";
-
 import CardContas from "../../components/cardContas";
 
 export default function Dash() {
   return (
-    <>
-      <p>Yuri JORGE</p>
-      <CardContas />
-    </>
+    <div className={styles.CardsContas}>
+      <CardContas value="150,00" type="agua" />
+      <CardContas value="200,00" type="energia" />
+      <CardContas value="500,00" type="condominio" />
+      <CardContas value="175,00" type="internet" />
+      <CardContas value="100,00" type="gas" />
+    </div>
   );
 }
+
+/* Notes:
+
+Quando estiver consumindo do banco de dados, criar função que identifica o type de conta do e já associa o icone correspondente  */
