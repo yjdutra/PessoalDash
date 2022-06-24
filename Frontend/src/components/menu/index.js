647ba29a46b4b807b50a9a572917ee1ba87d4377
YuriJorgeD <yjdutra@gmail.com>
2022-06-24 14:19:04 -0300

    [dev]: novaPagina

--- Frontend/src/components/menu/index.js
+++ Frontend/src/components/menu/index.js
@@ -1,6 +1,6 @@
 import styles from "./layout.module.css";
 
-import { casa, apps, predio, internet, fogo } from "../../assets/icons";
+import { casa, apps, ap, internet, fogo } from "../../assets/icons";
 
 export default function Menu(props) {
   return (
@@ -8,12 +8,17 @@ export default function Menu(props) {
       <nav className={styles.nav}>
         <ul className={styles.ul}>
           <li className={styles.li}>
-            <a href="http://localhost:3000/home" className={styles.a}>
+            <a href="http://localhost:3000/" className={styles.a}>
               <div className={styles.icon}>{casa}</div>
             </a>
           </li>
           <li className={styles.li}>
-          <a href="http://localhost:3000/apps" className={styles.a}>
+            <a href="http://localhost:3000/home" className={styles.a}>
+              <div className={styles.icon}>{ap}</div>
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="http://localhost:3000/apps" className={styles.a}>
               <div className={styles.icon}>{apps}</div>
             </a>
           </li>
